9229e04f33368009e26a8fce87ed5b778b50a80b
Mehran <mehranamanollah13@gmail.com>
2025-07-25 10:27:52 +0330

    title animation component updated

--- src/components/ui/TitleAnimation.tsx
+++ src/components/ui/TitleAnimation.tsx
@@ -12,7 +12,7 @@ export default function TitleAnimation({ text, animatedTexts, coloredTexts, clas
     const words = text.trim().split(" ")
 
     return (
-        <motion.h1 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.3 }} viewport={{ once: true }} className={className}>
+        <motion.h1 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.6 , delay: 0.2 }} viewport={{ once: true }} className={className}>
             {words.map((word, index) => {
                 const isAnimated = animatedTexts.includes(word);
                 const isColored = coloredTexts.includes(word);
@@ -22,15 +22,15 @@ export default function TitleAnimation({ text, animatedTexts, coloredTexts, clas
                 if (isAnimated) {
 
                     const delayIndex = animatedTexts.findIndex((t) => t === word);
-                    const delay = delayIndex !== -1 ? delayIndex * 0.1 : 0;
+                    const delay = delayIndex === 0 ? 0.3 : delayIndex * 0.3;
 
                     return (
                         <motion.span
                             key={index}
-                            initial={{ y: 5 , filter: "blur(5px)" }}
+                            initial={{ y: 7 , filter: "blur(5px)" }}
                             whileInView={{ y: 0 , filter: "blur(0)" }}
                             viewport={{ once: true }}
-                            transition={{ duration: 0.4, delay }}
+                            transition={{ duration: 0.3, delay }}
                             style={{ display: "inline-block" }}
                             className={classNames}
                         >
